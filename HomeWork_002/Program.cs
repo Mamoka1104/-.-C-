856a50d092b590b3c5a9b2834d0efcfe54224422
«Alisa» <mamoka1104@gmail.com>
2022-08-08 12:05:20 +1000

    add HomeWork_002

---
+++ HomeWork_002/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2; // 44, 22
+int b = 3; // 5, 3
+int c = 7; // 78, 9
+
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max); 
