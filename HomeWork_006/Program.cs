9027593fa9e390a6895452df3aecf45b866a976f
«Alisa» <mamoka1104@gmail.com>
2022-08-13 14:19:08 +1000

    add HomeWork_006

---
+++ HomeWork_006/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+
+
+
+Console.WriteLine("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = Math.Abs(num);
+string stringNum = Convert.ToString(a);
+
+if (stringNum.Length < 3)
+{
+    Console.WriteLine ("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine ("Третья цифра этого числа " + stringNum[2]);
+}
