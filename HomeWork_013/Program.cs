afd2d3af8db433b2063d60b811b17a3180814f1d
«Alisa» <mamoka1104@gmail.com>
2022-08-21 13:36:04 +1000

    add HomeWork_013

---
+++ HomeWork_013/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+static void WriteArray(int[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+    int[] array = new int[0];
+    while (true)
+    {
+        Console.WriteLine("Введите значение массива ");
+        string meaning = Console.ReadLine();
+        if(meaning != "end")
+        {
+            int number = Convert.ToInt32(meaning);
+            int[] newArray = array;
+            array = new int [array.Length + 1];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                array[i] = newArray[i];
+            } 
+            array[array.Length-1] = number; 
+        }
+        else 
+        {
+            break;
+        }
+    }
+    WriteArray(array);
+
