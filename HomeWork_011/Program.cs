877a4bb31864b17b1225515c63b0e6262d90fc4a
«Alisa» <mamoka1104@gmail.com>
2022-08-21 12:42:26 +1000

    add Добаваили условие задачи

--- HomeWork_011/Program.cs
+++ HomeWork_011/Program.cs
@@ -1,4 +1,8 @@
-﻿int number(string num)
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int number(string num)
 {
    Console.Write($"Введите число {num}: ");
    return Convert.ToInt32(Console.ReadLine());
