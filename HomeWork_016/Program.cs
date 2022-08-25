06bb5285a74feb11e6a378c247fdd51bbdb069e2
«Alisa» <mamoka1104@gmail.com>
2022-08-25 17:15:36 +1000

    add HomeWork_016

---
+++ HomeWork_016/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+int maxNumberArray(int[] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int mimNumberArray(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+{
+    int[]array = {3, 7, 22, 2, 78};
+    int max = maxNumberArray(array);
+    int min = mimNumberArray(array);
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+    Console.WriteLine(max - min);
+}
