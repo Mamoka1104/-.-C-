29d0e7fc0516e0593b2e19a96c1feda246ea7e24
«Alisa» <mamoka1104@gmail.com>
2022-08-25 16:32:31 +1000

    add HomeWork_014

---
+++ HomeWork_014/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+Random rand = new Random();
+int honestCount = 0;
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int numberCount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[numberCount];
+
+Console.Write("[");
+
+for (int i=0; i < numberCount; i++)
+{
+    array[i] = rand.Next(100, 999);
+    Console.Write(array[i]);
+    
+    if (i != numberCount -1)
+    {
+        Console.Write(", ");
+    }
+
+}
+
+Console.Write("]");
+
+for (int i = 0; i < numberCount; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        honestCount++;
+    }
+}
+Console.WriteLine("Количество четных значений - " + honestCount);
