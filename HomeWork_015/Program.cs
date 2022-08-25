97fe9a0f68009aff108c726d555d059849dc58a5
«Alisa» <mamoka1104@gmail.com>
2022-08-25 16:50:38 +1000

    add HomeWork_015

---
+++ HomeWork_015/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+
+Random rand = new Random();
+int sum = 0;
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+
+int numberCount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[numberCount];
+
+Console.Write("[");
+
+for (int i = 0; i < numberCount; i++)
+{
+    array[i] = rand.Next(100);
+    Console.Write(array[i]);
+    
+    if (i != numberCount - 1)
+    {
+        Console.Write(", ");
+    }
+
+}
+
+Console.Write("]");
+
+for (int k = 0 ; k < numberCount; k += 2)
+{
+    sum = sum + array[k];
+}
+Console.WriteLine("Сумма значений - " + sum);
